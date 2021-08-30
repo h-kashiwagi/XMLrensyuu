33d72833d349eaeaf07c399f7f08d91725068fca
h-kashiwagi <h-kashiwagi@fpcom.jp>
2021-08-30 17:29:45 +0900

    Form2

--- XMLrennsyuu/XMLrennsyuu/Form1.cs
+++ XMLrennsyuu/XMLrennsyuu/Form1.cs
@@ -25,7 +25,45 @@ namespace XMLrennsyuu
 
         private void button1_Click(object sender, EventArgs e)
         {
+            listView1.Items.Clear();
+            XmlDocument myDoc = new XmlDocument();
+            myDoc.Load(@"C:\Users\h-kashiwagi\Desktop\サテライトオフィス\XMLrennsyuu\XMLrennsyuu\XML\Cdlist.xml");
+            XmlNode rootElement = myDoc.DocumentElement;
             
+            //ChildNodesプロパティによってXmlNodeListオブジェクトを取得
+            foreach(XmlNode currentNode in rootElement.ChildNodes)
+            {
+                //currentNodeの種類が要素でかつ、currentNodeの名前がCDの場合==CD要素のNodeの場合
+                if(currentNode.NodeType == XmlNodeType.Element && currentNode.Name == "CD")
+                {
+                    //リストビューに表示するための準備
+                    ListViewItem lvItem = new ListViewItem();
+                    lvItem.SubItems.AddRange(new string[] { "", "" });
+
+                    //CD要素の子ノードのリストから値を取り出す
+                    foreach (XmlNode subNode in currentNode.ChildNodes)
+                    {
+                        //子ノードの中のノードの種類が要素の場合
+                        if (subNode.NodeType == XmlNodeType.Element)
+                        
+                            switch (subNode.Name)
+                            {
+                                case "Title":
+                                    lvItem.SubItems[0].Text = subNode.InnerText; //内容を変数にセット
+                                    break;
+                                case "Artist":
+                                    lvItem.SubItems[1].Text = subNode.InnerText;
+                                    break;
+                                case "ReleaseData":
+                                    lvItem.SubItems[2].Text = subNode.InnerText;
+                                    break;
+                            }
+                        
+                    }
+                    listView1.Items.Add(lvItem);
+                }
+
+            }
         }
 
         
---
+++ XMLrennsyuu/XMLrennsyuu/Form2.Designer.cs
@@ -0,0 +1,217 @@
+﻿
+namespace XMLrennsyuu
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.goButtan = new System.Windows.Forms.Button();
+            this.曲名 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // goButtan
+            // 
+            this.goButtan.Location = new System.Drawing.Point(348, 30);
+            this.goButtan.Name = "goButtan";
+            this.goButtan.Size = new System.Drawing.Size(88, 23);
+            this.goButtan.TabIndex = 0;
+            this.goButtan.Text = "読み込み";
+            this.goButtan.UseVisualStyleBackColor = true;
+            // 
+            // 曲名
+            // 
+            this.曲名.AutoSize = true;
+            this.曲名.Location = new System.Drawing.Point(59, 171);
+            this.曲名.Name = "曲名";
+            this.曲名.Size = new System.Drawing.Size(37, 15);
+            this.曲名.TabIndex = 1;
+            this.曲名.Text = "曲名";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 15;
+            this.listBox1.Location = new System.Drawing.Point(47, 30);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(251, 109);
+            this.listBox1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(59, 211);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(37, 15);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "作曲";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(59, 255);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 15);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "作詞";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(59, 295);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(37, 15);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "編曲";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(59, 334);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(67, 15);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "演奏時間";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(59, 372);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(55, 15);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "アルバム";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(211, 171);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(23, 15);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "\"\"";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(211, 211);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(23, 15);
+            this.label7.TabIndex = 9;
+            this.label7.Text = "\"\"";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(211, 254);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(23, 15);
+            this.label8.TabIndex = 10;
+            this.label8.Text = "\"\"";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(211, 294);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(23, 15);
+            this.label9.TabIndex = 11;
+            this.label9.Text = "\"\"";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(211, 333);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(23, 15);
+            this.label10.TabIndex = 12;
+            this.label10.Text = "\"\"";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(214, 371);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(23, 15);
+            this.label11.TabIndex = 13;
+            this.label11.Text = "\"\"";
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(482, 453);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.曲名);
+            this.Controls.Add(this.goButtan);
+            this.Name = "Form2";
+            this.Text = "曲のリスト";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button goButtan;
+        private System.Windows.Forms.Label 曲名;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+    }
+}
